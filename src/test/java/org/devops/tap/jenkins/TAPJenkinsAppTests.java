7a5795813bdeab2aa60be25781f92185b8ca1675
dkhopade <deepak.khopade@gmail.com>
2023-06-07 23:02:54 -0400

    added tests

--- src/test/java/org/devops/tap/jenkins/TAPJenkinsAppTests.java
+++ src/test/java/org/devops/tap/jenkins/TAPJenkinsAppTests.java
@@ -1,13 +1,33 @@
 package org.devops.tap.jenkins;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(TAPJenkinsApp.class)
 public class TAPJenkinsAppTests {
 
+  @Autowired
+  private TAPJenkinsApp controller;
+
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  void contextLoads() {
+  void index() throws Exception {
+    assertEquals("Welcome to TAP SupplyChains with Jenkins", controller.message());
+
+    mockMvc
+        .perform(get("/"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Welcome to TAP SupplyChains with Jenkins"));
   }
 
 }
